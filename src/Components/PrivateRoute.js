f7c0bb420e4368ec97639cc0c37b782bf10e6595
Tommy Yu <tombahyu@gmail.com>
2021-09-23 15:21:03 -0400

    small routing fix

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -11,7 +11,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         <Route
             {...rest}
             render={props => {
-                return currentUser ? <Component {...props} /> : <Redirect to='/login' />
+                return currentUser ? <Component {...props} /> : <Redirect to='/' />
             }}
         >
         </Route>
